73fd4e081a14b1c91adb4df6440b73d7bd7e9960
Hoc Lap Trinh <vtmbmt1125@gmail.com>
2022-12-27 13:27:51 +0700

    deploy netlify commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { HashRouter as Router} from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
@@ -12,9 +13,9 @@ import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-   
+   <Router>
     <App />
-   
+    </Router>
   </React.StrictMode>
 );
 
