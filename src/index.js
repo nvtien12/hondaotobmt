4cd60d25791e8e63e1c0cb9373b6d3b5695231f5
Hoc Lap Trinh <vtmbmt1125@gmail.com>
2022-12-27 13:39:16 +0700

    tesst deylop second commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import {BrowserRouter as Router} from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
@@ -13,9 +13,9 @@ import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-   <Router>
+   <BrowserRouter>
     <App />
-    </Router>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
