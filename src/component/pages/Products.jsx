9230828e0d3d249dfe6ecd111d1bdb98c32c1e20
Hoc Lap Trinh <vtmbmt1125@gmail.com>
2023-01-12 18:10:23 +0700

    url 2 commit

--- src/component/pages/Products.jsx
+++ src/component/pages/Products.jsx
@@ -10,7 +10,7 @@ export default function Products() {
     useEffect(() => {
         const getProducts = async() =>{
             setLoading(true);
-            const response =await fetch("https://raw.githubusercontent.com/nvtien12/my-react/main/database/db.json");
+            const response =await fetch("http://localhost:8000/Honda")
             if(componentMounted){
                 setData(await response.clone().json());
                 setFilter(await response.json());
